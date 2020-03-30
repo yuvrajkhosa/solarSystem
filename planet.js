8f1e505325e4af8b20d1f9dc0f795202e2f920c8
Yuvraj <uvkhosa@yahoo.com>
2020-03-30 02:49:48 -0700

    V2

--- planet.js
+++ planet.js
@@ -1,10 +1,10 @@
 class Planet{
-    constructor(mass ,distFromSun, initAccelX, initAccelY, diam, timeForAccel){
+    constructor(mass ,distFromSun, initAccelX, initAccelY, diam, timeForAccel, posX, posY){
         this.mass = mass;
         this.diam = diam;
         this.timeForAccel = timeForAccel;
         this.initAccel = createVector(initAccelX, initAccelY);
-        this.pos = createVector(width / 2, height / 2 - distFromSun);
+        this.pos = !posX ? createVector(width / 2, height / 2 - distFromSun) : createVector(posX, posY);//If no posX and posY given, do radius from sun mode
         this.accel = createVector(0,0);
         this.vel = createVector(0,0);
         this.colour = [random(255), random(255), random(255)];
---
+++ star.js
@@ -0,0 +1,7 @@
+class Star{
+    constructor(posX, posY, diam, mass){
+        this.pos = createVector(posX, posY);
+        this.diam = diam;
+        this.mass = mass;
+    }
+}
